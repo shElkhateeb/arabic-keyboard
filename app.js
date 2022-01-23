9edbafd45d0e421bf3f41b4850fd5739c9869edf
Shorouk Elkhateeb <shrouca@gmail.com>
2022-01-23 17:21:16 +0200

    Display text in text area

---
+++ app.js
@@ -0,0 +1,15 @@
+const keyboard = document.getElementById('keyboard');
+const textArea = document.getElementById('textArea');
+
+// Add text to text area
+function addText(event){
+    if(event.target.id == 'space') {
+        textArea.textContent += ' ';
+    }
+    else {
+        textArea.textContent += event.target.textContent;
+    }  
+}
+
+// Listen for pressing keyboard event
+keyboard.addEventListener('click', addText);
