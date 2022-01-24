a03202d519a81eb37c7a76d3729bb17e9859e04e
Shorouk Elkhateeb <shrouca@gmail.com>
2022-01-24 09:06:46 +0200

    Add shift key

--- app.js
+++ app.js
@@ -3,15 +3,23 @@ const textArea = document.getElementById('textArea');
 
 // Add text to text area
 function addText(event){
+    // Make sure key is pressed
     if(event.target.tagName === 'BUTTON'){
-        if(event.target.id == 'space') {
+        // Case space is pressed
+        if(event.target.id === 'space') {
             textArea.textContent += ' ';
         }
+        // Case shift is pressed
+        else if(event.target.id === 'shift') {
+
+        }
+        // Case a letter is pressed
         else {
+            // Add this letter to text area
             textArea.textContent += event.target.textContent;
         }  
     }
 }
 
-// Listen for pressing keyboard event
+// Listen for pressing-keyboard event
 keyboard.addEventListener('click', addText);
