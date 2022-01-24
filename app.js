93053bc248237f8aee57ce2243c72b737c080834
Shorouk Elkhateeb <shrouca@gmail.com>
2022-01-24 11:27:32 +0200

    Add up keyboard

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 const keyboard = document.getElementById('keyboard');
+const keyboardUp = document.getElementById('keyboard-up');
+
+const shiftButtonOn = document.getElementById('shift-on');
+const shiftButtonOff = document.getElementById('shift-off');
+
 const textArea = document.getElementById('textArea');
 
 // Add text to text area
 function addText(event){
     // Make sure key is pressed
     if(event.target.tagName === 'BUTTON'){
-        // Case space is pressed
-        if(event.target.id === 'space') {
-            textArea.textContent += ' ';
+        // Case shift is unpressed
+        // Dispay normal keyboard
+        if(event.target === shiftButtonOn) {
+            keyboardUp.style.display = 'none';
+            keyboard.style.display = 'grid';
         }
         // Case shift is pressed
-        else if(event.target.id === 'shift') {
-
+        else if(event.target === shiftButtonOff) {
+            keyboard.style.display = 'none';
+            keyboardUp.style.display = 'grid';
         }
+        // Case space is pressed
+        else if(event.target.className === 'space') {
+            textArea.textContent += ' ';
+        }
+        
         // Case a letter is pressed
         else {
             // Add this letter to text area
@@ -23,3 +36,4 @@ function addText(event){
 
 // Listen for pressing-keyboard event
 keyboard.addEventListener('click', addText);
+keyboardUp.addEventListener('click', addText);
